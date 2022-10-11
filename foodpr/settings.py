74c5cb37fd2eb4bed1e06b3e6c0633a2ad9d5701
Vikas <vikas1618072@gmail.com>
2022-10-11 20:24:33 +0800

    edited in allowed host

--- foodpr/settings.py
+++ foodpr/settings.py
@@ -30,9 +30,9 @@ chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
 SECRET_KEY = get_random_string(50, chars)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["online-food-ordering-system-production.up.railway.app"]
 
 
 # Application definition
